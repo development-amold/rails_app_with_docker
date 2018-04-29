c9f9e434c0a63f9b077573eacd0276622b3296e7
Amol Dumbre <dev.amold@gmail.com>
2018-04-29 15:29:16 +0530

    Added API for angular5 app

---
+++ config/routes.rb
@@ -0,0 +1,5 @@
+Rails.application.routes.draw do
+  resources :coins
+  get "coins/:id/edit" => "coins#edit"
+  # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+end
